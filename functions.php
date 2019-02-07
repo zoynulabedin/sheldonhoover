454929e353870798e3a6e9d53afa76423a3df500
zoynulabedin <trustcodernet@gmail.com>
2019-02-08 02:43:20 +0600

    header style

--- functions.php
+++ functions.php
@@ -8,15 +8,41 @@ Note: this function loads the parent stylesheet before, then child theme stylesh
 (leave it in place unless you know what you are doing.)
 */
 
+if ( ! function_exists( 'divi_child_setup' ) ) :
+
+	function divi_child_setup() {
+
+		register_nav_menus( array(
+			'child_menu' => esc_html__( 'Main Menu', 'sheldonhoover' ),
+		
+		) );
+
+	}
+endif;
+add_action( 'after_setup_theme', 'divi_child_setup' );
+
+
 function sheldonhoover_enqueue_child_styles() {
 $parent_style = 'parent-style'; 
+	wp_enqueue_style('bootstrap-css', get_stylesheet_directory_uri() . '/assets/css/bootstrap.min.css' );
+	wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/assets/css/font-awesome.min.css' );
 	wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 
 		'child-style', 
 		get_stylesheet_directory_uri() . '/style.css',
 		array( $parent_style ),
 		wp_get_theme()->get('Version') );
+		wp_enqueue_script( 'bootsrap-js',  get_stylesheet_directory_uri(). '/assets/js/bootstrap.min.js', array( 'jquery' ), null, true );
 	}
 add_action( 'wp_enqueue_scripts', 'sheldonhoover_enqueue_child_styles' );
+
+
+/*** ACF OPTIONS PAGE */
+if(function_exists('acf_add_options_page')) {
+	acf_add_options_page(array(
+		'page_title' => 'Divi Header',
+		'icon_url' => get_stylesheet_directory_uri().'/assets/img/divi.png',
+	));
+  }
 
 /*Write here your own functions */
---
+++ header.php
@@ -0,0 +1,99 @@
+<?php 
+$social_links = get_field('social_links','options');
+?>
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<?php elegant_description(); ?>
+	<?php elegant_keywords(); ?>
+	<?php elegant_canonical(); ?>
+
+	<?php do_action( 'et_head_meta' ); ?>
+
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<?php $template_directory_uri = get_template_directory_uri(); ?>
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( $template_directory_uri . '/js/html5.js"' ); ?>" type="text/javascript"></script>
+	<![endif]-->
+
+	<script type="text/javascript">
+		document.documentElement.className = 'js';
+	</script>
+
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<div class="header-child">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="header-left">
+					<?php if( get_field('logo_title','options') ): ?>
+						<h2><?php the_field('logo_title','options'); ?></h2>
+					<?php endif; ?>
+					<?php if( get_field('sub_title','options') ): ?>
+						<p><?php the_field('sub_title','options'); ?></p>
+					<?php endif; ?>
+						
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="child-main-menu">
+						<?php 
+							wp_nav_menu([
+								'theme_location'	=>'child_menu',
+							]);
+						?>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="social-icon">
+						<ul>
+						<?php if ( $social_links['facebook'] ) : ?>
+						<li><a href="<?php echo $social_links['facebook']; ?>"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+						<?php endif; ?>
+
+						<?php if ( $social_links['twitter'] ) : ?>
+						<li><a href="<?php echo $social_links['twitter']; ?>"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+						<?php endif; ?>
+
+						<?php if ( $social_links['youtube'] ) : ?>
+						<li><a href="<?php echo $social_links['youtube']; ?>"><i class="fa fa-youtube-square" aria-hidden="true"></i></a></li>
+						<?php endif; ?>
+
+						<?php if ( $social_links['rss'] ) : ?>
+						<li><a href="<?php echo $social_links['rss']; ?>"><i class="fa fa-rss-square" aria-hidden="true"></i></a></li>
+						<?php endif; ?>
+					
+						
+						</ul>
+					</div>
+					<div class="phone_number">
+							<?php if ( get_field('phone_number','options') ) : ?>
+								<?php echo get_field('phone_number','options'); ?>
+							<?php endif; ?>
+							
+					</div>
+				</div>
+				<?php if ( get_field('upload_image','options') ) : ?>
+					<div class="client-img">
+					<img src="<?php the_field('upload_image','options'); ?>" alt="<?php the_field(''); ?>">
+				<?php endif; ?>
+					</div>
+				
+			</div>
+		</div>	
+	</div>
